c54294f5867433774d25a557e248f84304ffe9bb
tirth1632 <tirthpatel1632@gmail.com>
2024-11-25 08:37:24 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print meow meowq
+n=int(input("no of times leading party own the election : ")
+m=int(input("no of times opposeing party own the election : ")
+print("predicting the output ...")
+for i in range(0,1000000000000000):
+      print(" ",end='')
+if(n>m):
+  print("ruling party will win from past election")
+else:
+  print("opposeing party will win from past election")
+
