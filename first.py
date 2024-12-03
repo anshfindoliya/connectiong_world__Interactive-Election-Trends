7d385963fcc7d7fb1d922b70cb13c687fb64835e
anshfindoliya <168640161+anshfindoliya@users.noreply.github.com>
2024-12-03 22:34:26 +0530

    Delete first.py

--- first.py
+++
@@ -1,9 +0,0 @@
-n=int(input("no of times leading party own the election : "))
-m=int(input("no of times opposeing party own the election : "))
-print("predicting the output ...")
-for i in range(0,10000000):
-      print("",end='')
-if(n>m):
-  print("ruling party will win from past election")
-else:
-  print("opposeing party will win from past election")
